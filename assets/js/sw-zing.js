c21ca86e45fb3fec5e35da120516d0240b7996eb
Lukey <lshort2@mail.csuchico.edu>
2017-08-24 16:56:45 -0700

    added more ships

--- assets/js/sw-zing.js
+++ assets/js/sw-zing.js
@@ -54,3 +54,227 @@ zingchart.render({
   width: '100%' 
 });
 
+var etaConfig = {
+  type : 'radar',
+  plot : {
+    aspect : 'area',
+    animation: {
+      effect:3,
+      sequence:1,
+      speed:700
+    }
+  },
+  scaleV : {
+    visible : false
+  },
+  scaleK : {
+    values : '0:4:1',
+    labels : ['Firepower','Speed','Hyperdrive','Cargo Capacity', 'Cost' ],
+    item : {
+      fontColor : '#607D8B',
+      backgroundColor : "white",
+      borderColor : "#aeaeae",
+      borderWidth : 1,
+      padding : '5 10',
+      borderRadius : 10
+    },
+    refLine : {
+      lineColor : '#c10000'
+    },
+    tick : {
+      lineColor : '#59869c',
+      lineWidth : 2,
+      lineStyle : 'dotted',
+      size : 20
+    },
+    guide : {
+      lineColor : "#607D8B",
+      lineStyle : 'solid',
+      alpha : 0.3,
+      backgroundColor : "#c5c5c5 #718eb4"
+    }
+  },
+  series : [
+    {
+      values : [20, 20, 54, 41, 41],
+      lineColor : '#BE2436',
+      backgroundColor : '#F4495D'
+    }
+  ]
+};
+
+zingchart.render({ 
+  id : 'eta2_chart', 
+  data : etaConfig, 
+  height: '100%', 
+  width: '100%' 
+});
+
+var tieConfig = {
+  type : 'radar',
+  plot : {
+    aspect : 'area',
+    animation: {
+      effect:3,
+      sequence:1,
+      speed:700
+    }
+  },
+  scaleV : {
+    visible : false
+  },
+  scaleK : {
+    values : '0:4:1',
+    labels : ['Firepower','Speed','Hyperdrive','Cargo Capacity', 'Cost' ],
+    item : {
+      fontColor : '#607D8B',
+      backgroundColor : "white",
+      borderColor : "#aeaeae",
+      borderWidth : 1,
+      padding : '5 10',
+      borderRadius : 10
+    },
+    refLine : {
+      lineColor : '#c10000'
+    },
+    tick : {
+      lineColor : '#59869c',
+      lineWidth : 2,
+      lineStyle : 'dotted',
+      size : 20
+    },
+    guide : {
+      lineColor : "#607D8B",
+      lineStyle : 'solid',
+      alpha : 0.3,
+      backgroundColor : "#c5c5c5 #718eb4"
+    }
+  },
+  series : [
+    {
+      values : [20, 20, 54, 41, 41],
+      lineColor : '#BE2436',
+      backgroundColor : '#F4495D'
+    }
+  ]
+};
+
+zingchart.render({ 
+  id : 'tie_chart', 
+  data : tieConfig, 
+  height: '100%', 
+  width: '100%' 
+});
+
+var nabooConfig = {
+  type : 'radar',
+  plot : {
+    aspect : 'area',
+    animation: {
+      effect:3,
+      sequence:1,
+      speed:700
+    }
+  },
+  scaleV : {
+    visible : false
+  },
+  scaleK : {
+    values : '0:4:1',
+    labels : ['Firepower','Speed','Hyperdrive','Cargo Capacity', 'Cost' ],
+    item : {
+      fontColor : '#607D8B',
+      backgroundColor : "white",
+      borderColor : "#aeaeae",
+      borderWidth : 1,
+      padding : '5 10',
+      borderRadius : 10
+    },
+    refLine : {
+      lineColor : '#c10000'
+    },
+    tick : {
+      lineColor : '#59869c',
+      lineWidth : 2,
+      lineStyle : 'dotted',
+      size : 20
+    },
+    guide : {
+      lineColor : "#607D8B",
+      lineStyle : 'solid',
+      alpha : 0.3,
+      backgroundColor : "#c5c5c5 #718eb4"
+    }
+  },
+  series : [
+    {
+      values : [20, 20, 54, 41, 41],
+      lineColor : '#BE2436',
+      backgroundColor : '#F4495D'
+    }
+  ]
+};
+
+zingchart.render({ 
+  id : 'n1_chart', 
+  data : nabooConfig, 
+  height: '100%', 
+  width: '100%' 
+});
+
+var vultureConfig = {
+  type : 'radar',
+  plot : {
+    aspect : 'area',
+    animation: {
+      effect:3,
+      sequence:1,
+      speed:700
+    }
+  },
+  scaleV : {
+    visible : false
+  },
+  scaleK : {
+    values : '0:4:1',
+    labels : ['Firepower','Speed','Hyperdrive','Cargo Capacity', 'Cost' ],
+    item : {
+      fontColor : '#607D8B',
+      backgroundColor : "white",
+      borderColor : "#aeaeae",
+      borderWidth : 1,
+      padding : '5 10',
+      borderRadius : 10
+    },
+    refLine : {
+      lineColor : '#c10000'
+    },
+    tick : {
+      lineColor : '#59869c',
+      lineWidth : 2,
+      lineStyle : 'dotted',
+      size : 20
+    },
+    guide : {
+      lineColor : "#607D8B",
+      lineStyle : 'solid',
+      alpha : 0.3,
+      backgroundColor : "#c5c5c5 #718eb4"
+    }
+  },
+  series : [
+    {
+      values : [20, 20, 54, 41, 41],
+      lineColor : '#BE2436',
+      backgroundColor : '#F4495D'
+    }
+  ]
+};
+
+zingchart.render({ 
+  id : 'vulture_chart', 
+  data : vultureConfig, 
+  height: '100%', 
+  width: '100%' 
+});
+
